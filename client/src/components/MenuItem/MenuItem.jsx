24fe7e89f5696723fd17359dbaba30527753ae14
Glenn Montera <glenn.montera@gmail.com>
2023-12-03 10:25:38 -0500

    add styling to menuitem

--- client/src/components/MenuItem/MenuItem.jsx
+++ client/src/components/MenuItem/MenuItem.jsx
@@ -4,7 +4,7 @@ function MenuItem(props) {
   return (
     <div className="menu-item">
       <div>image</div>
-      <div>
+      <div className="menu-item__text">
         <h3>Guacamole and Chips</h3>
         <h3>$11.00</h3>
         <p>Fresh Avacado, Tomatoes, Onions, Cilantro, and Lime</p>
