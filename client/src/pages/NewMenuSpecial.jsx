0902c494ef70fbc1e7a71ece267131298e19c12a
Himja Parekh <himjaparekh2000@gmail.com>
2023-12-04 20:44:53 -0500

    typography add

--- client/src/pages/NewMenuSpecial.jsx
+++ client/src/pages/NewMenuSpecial.jsx
@@ -1,6 +1,8 @@
+import "../assets/styles/newMenuSpecial.scss";
+
 function NewMenuSpecial() {
   return (
-    <div>
+    <div className="special__main">
       <div>
         <h3>Special</h3>
         <p>Menu / Current Menu / Create New Menu Item / Special</p>
@@ -8,15 +10,31 @@ function NewMenuSpecial() {
       <div>
         <h4>Recommend Items</h4>
         <div className="item-img"></div>
-          </div>
-          
-          <div>
-              <h4>New Items</h4>  
-              <form>
-                  <div className="uploadImg"></div>
-                  
-              </form>
-          </div>
+      </div>
+
+      <div>
+        <h4>New Items</h4>
+        <div>
+          <div className="uploadImg"></div>
+          <form>
+            <div>
+              <label>Item Name</label>
+              <input type="text" name="itemName" placeholder="Item Name" />
+              <label>Item Price</label>
+              <input type="text" name="itemPrice" placeholder="Item Price" />
+            </div>
+            <label>Description</label>
+            <textarea
+              name="itemDescription"
+              id=""
+              cols="30"
+              rows="10"
+              placeholder="Item Description"
+            ></textarea>
+          </form>
+          <button>Add</button>
+        </div>
+      </div>
     </div>
   );
 }
