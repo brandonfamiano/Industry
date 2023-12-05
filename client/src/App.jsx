0bd6a651091932c6fbf62caf555ee3a8e811cf82
Zsoreign <zrsanchez36@gmail.com>
2023-12-04 20:44:13 -0500

    Quick fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import './App.css'
+// import './App.css'
 
 // Page Components
 import NewMenuItem from './pages/new_menuitem/NewMenuItem';
